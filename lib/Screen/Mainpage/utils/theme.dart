10abebf9ecdb4bca5456f3c0f55dc54f4dad7f29
xxerone <gkstjd6097@gmail.com>
2024-11-24 18:05:40 +0900

    폰트 관련 theme.dart 생성

---
+++ lib/Screen/Mainpage/utils/theme.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+// 그레이 색상
+const greyColor1=Color(0xFFA7AFC0);
+const greyColor2=Color(0xFF98A1B4);
+const greyColor3=Color(0xFFE0E4EB);
+const greyColor4=Color(0xFF6D7382);
+const greyColor5=Color(0xFFB6B6B6);
+const greyColor6=Color(0xFFA4AEC2);
+const greyColor7=Color(0xFFD7DEED);
+
+const TextStyle3 = TextStyle(
+  // fontSize: 16,
+  // color: greyColor1,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w300, // Bold
+);
+
+const TextStyle4 = TextStyle(
+  // fontSize: 16,
+  // color: greyColor2,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w400, // Medium
+  // decoration: TextDecoration.underline,
+);
+
+const TextStyleSub4 = TextStyle(
+  fontSize: 16,
+  // color: greyColor2,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w400, // Medium
+  // decoration: TextDecoration.underline,
+);
+
+const TextStyle5 = TextStyle(
+  // fontSize: 16,
+  // color: greyColor1,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w500, // Bold
+);
+
+const TextStyleSub5 = TextStyle(
+  // fontSize: 16,
+  color: Colors.grey,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w500, // Bold
+);
+
+const TextStyle6 = TextStyle(
+  // fontSize: 16,
+  // color: greyColor2,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w600, // Medium
+  // decoration: TextDecoration.underline,
+);
+
+const TextStyleMain6 = TextStyle(
+  // fontSize: 16,
+  // color: greyColor2,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w600, // Medium
+  // decoration: TextDecoration.underline,
+);
+
+const TextStyleRating6 = TextStyle(
+  fontSize: 20,
+  // color: greyColor2,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w600, // Medium
+  // decoration: TextDecoration.underline,
+);
+
+const TextStyleSub6 = TextStyle(
+  fontSize: 18,
+  // color: greyColor2,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w600, // Medium
+  // decoration: TextDecoration.underline,
+);
+
+const TextStyle7 = TextStyle(
+  fontSize: 16,
+  // color: greyColor1,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w700, // Bold
+);
+
+const TextStyleMain7 = TextStyle(
+  fontSize: 20,
+  // color: greyColor1,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w700, // Bold
+);
+
+const TextStyle8 = TextStyle(
+  // fontSize: 16,
+  // color: greyColor2,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w800, // Medium
+  // decoration: TextDecoration.underline,
+);
+
+const TextStyleMain8 = TextStyle(
+  fontSize: 24,
+  // color: greyColor1,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w800, // Bold
+);
