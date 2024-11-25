e24540f55ad40c6bdfc894b5a53aadc91bddba48
wldsmdl7 <wldmsdl5835@gmail.com>
2024-11-25 19:33:27 +0900

    사용자 리뷰 수정

--- lib/Screen/Restaurant/Restaurant.dart
+++ lib/Screen/Restaurant/Restaurant.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:kksc_app_fe/Screen/Restaurant/RestaurantList.dart';
+import 'package:kksc_app_fe/Component/LabelCard.dart';
 
 class Restaurant extends StatelessWidget {
   final List<Map<String, String>> cardData = [
@@ -15,9 +18,9 @@ class Restaurant extends StatelessWidget {
   ];
 
   final List<Map<String, String>> reviewData = [
-    {"user": "사용자1", "content": "맛있는 음식이었어요"},
-    {"user": "사용자2", "content": "별로였어요"},
-    {"user": "사용자3", "content": "음 굿"}
+    {"user": "사용자1", "content": "맛있는 음식이었어요", "starCount": "5"},
+    {"user": "사용자2", "content": "별로였어요", "starCount": "1"},
+    {"user": "사용자3", "content": "음 굿", "starCount": "3"}
   ];
 
   @override
@@ -144,16 +147,48 @@ class Restaurant extends StatelessWidget {
                                 : 4, // 마지막 카드의 오른쪽 간격 없음
                           ),
                           child: ReviewCard(
-                            user: item["user"]!,
-                            content: item["content"]!,
-                          ),
+                              user: item["user"]!,
+                              content: item["content"]!,
+                              starCount: item["starCount"]!),
                         );
                       },
                     ))
               ],
             ),
           ),
-        )
+        ),
+        SliverPadding(
+          padding: EdgeInsets.only(right: 12, left: 12, top: 8),
+          sliver: SliverToBoxAdapter(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GetTitle(title: "맛집 소식"),
+              SizedBox(
+                height: 8,
+              ),
+              SizedBox(
+                height: 250, // 고정된 가로 스크롤 영역 높이
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: cardData.length,
+                  itemBuilder: (context, index) {
+                    final item = cardData[index];
+                    return Padding(
+                      padding: EdgeInsets.only(
+                        left: index == 0 ? 0 : 4, // 첫 번째 카드의 왼쪽 간격 없음
+                        right: index == cardData.length - 1
+                            ? 0
+                            : 4, // 마지막 카드의 오른쪽 간격 없음
+                      ),
+                      //child:
+                    );
+                  },
+                ),
+              ),
+            ],
+          )),
+        ),
       ],
     );
   }
@@ -257,115 +292,13 @@ class NavigateButton extends StatelessWidget {
   }
 }
 
-class LabelCard extends StatelessWidget {
-  final String? label;
-  final String? img;
-  final String name;
-  final String text;
-
-  const LabelCard(
-      {this.label, this.img, required this.name, required this.text});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 164, //카드 너비
-      height: 224, //카드 높이
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(6)),
-        border: Border.all(
-          color: const Color.fromRGBO(0, 0, 0, 0.1),
-          width: 1,
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Stack(
-            children: [
-              Container(
-                  height: 164,
-                  color: Color.fromRGBO(0, 0, 0, 0.05),
-                  child: Image.asset(
-                    fit: BoxFit.cover,
-                    img!,
-                    errorBuilder: (context, error, stackTrace) {
-                      // 이미지 로드 실패 시 "No Image" 표시
-                      return const Center(
-                        child: Text(
-                          "No Image",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      );
-                    },
-                  )),
-              if (label != null && label!.isNotEmpty)
-                Positioned(
-                  top: 0,
-                  left: 0,
-                  child: Container(
-                    width: 31,
-                    height: 24,
-                    padding: const EdgeInsets.all(4),
-                    decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(6),
-                          bottomRight: Radius.circular(6),
-                        ),
-                        // 기존 색 : color: Color.fromRGBO(0, 0, 0, 0.1), -> label이 잘 안 보임
-                        color: Color.fromRGBO(0, 0, 0, 0.5)),
-                    child: Text(
-                      label!,
-                      style: const TextStyle(
-                        //label text 잘 안 보여서 색 변경
-                        color: Colors.white,
-                        fontSize: 12,
-                        height: 16 / 12,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                ),
-            ],
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(name,
-                      style: const TextStyle(
-                          fontSize: 12,
-                          height: 16 / 12,
-                          fontWeight: FontWeight.w400),
-                      textAlign: TextAlign.start),
-                  Text(
-                    text,
-                    style: const TextStyle(
-                        fontSize: 16,
-                        height: 24 / 16,
-                        fontWeight: FontWeight.w500),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
 class ReviewCard extends StatelessWidget {
   final String user;
   final String content;
+  final String starCount;
 
-  const ReviewCard({required this.user, required this.content});
+  const ReviewCard(
+      {required this.user, required this.content, required this.starCount});
 
   @override
   Widget build(BuildContext context) {
@@ -404,31 +337,18 @@ class ReviewCard extends StatelessWidget {
                         fontWeight: FontWeight.w500),
                   ),
                 ),
-                const Icon(
-                  Icons.star,
-                  size: 12,
-                  color: Color.fromRGBO(255, 199, 0, 1),
-                ),
-                const Icon(
-                  Icons.star,
-                  size: 12,
-                  color: Color.fromRGBO(255, 199, 0, 1),
-                ),
-                const Icon(
-                  Icons.star,
-                  size: 12,
-                  color: Color.fromRGBO(255, 199, 0, 1),
-                ),
-                const Icon(
-                  Icons.star,
-                  size: 12,
-                  color: Color.fromRGBO(255, 199, 0, 1),
-                ),
-                const Icon(
-                  Icons.star,
-                  size: 12,
-                  color: Color.fromRGBO(255, 199, 0, 1),
-                ),
+                for (int i = 0; i < int.parse(starCount); i++)
+                  const Icon(
+                    Icons.star,
+                    size: 12,
+                    color: Color.fromRGBO(255, 199, 0, 1),
+                  ),
+                for (int i = 0; i < 5 - int.parse(starCount); i++)
+                  const Icon(
+                    Icons.star,
+                    size: 12,
+                    color: Color.fromRGBO(0, 0, 0, 0.3),
+                  ),
               ],
             ),
           ),
