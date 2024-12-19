736488295f32efbb79b37cf0dee55f36af60d88c
xxerone <gkstjd6097@gmail.com>
2024-12-20 00:23:14 +0900

    api 관련 코드 추가

---
+++ lib/util/api/Services/get_services.dart
@@ -0,0 +1,14 @@
+import 'package:projecr_kksc_gachon_gil_project_recent_flutter_project/util/api/Model/get_model.dart';
+import 'package:http/http.dart' as http;
+
+class GetApiServices {
+  Future <List <CommandsModel>?> getCommentsModel() async {
+    var data = http.Client();
+    var uri = Uri.parse("https://jsonplaceholder.typicode.com/comments");
+    var response = await data.get(uri);
+    if(response.statusCode == 200) {
+      var json = response.body;
+      return
+    }
+  }
+}
