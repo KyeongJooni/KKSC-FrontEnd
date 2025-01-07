58410cef231bf44cc31da3d4905a1a9770113a1f
wldsmdl7 <wldmsdl5835@gmail.com>
2025-01-07 11:39:06 +0900

    google map api 연동 (IOS) - api key는 보안을 위해 각자 입력 (추후 공지) - api 코드 받았을 경우 android/app/src/main/AndroidManifest.xml의 <application> 태그 안에 추가하기 - Android - api 코드 받았을 경우 	1. ios/Runner/AppDelegate.swift 에 다음 코드 입력 		import GoogleMaps 		... 		-> Bool { 		GMSServices.provideAPIKey("{API KEY}") 		... 		} 	2. cocoapods 설치 (구글링하여 설치)

--- lib/Screen/Restaurant/RestaurantListIn.dart
+++ lib/Screen/Restaurant/RestaurantListIn.dart
@@ -1,34 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:kksc_app_fe/Component/TopAppbar.dart';
-import 'package:kksc_app_fe/Component/BottomNavbar.dart';
-import 'package:kksc_app_fe/util/text_styles.dart';
 
 class RestaurantView extends StatelessWidget {
   const RestaurantView({super.key});
 
-  void _onlogopressed() {}
-
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              if (Navigator.canPop(context)) {
-                Navigator.pop(context);
-              } else {
-                print("이전화면이 없습니다.");
-              }
-            },
-          ),
-          title: Text("내부 식당", style: textTheme.headlineMedium),
-          actions: [SizedBox(), SizedBox()],
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            if (Navigator.canPop(context)) {
+              Navigator.pop(context);
+            } else {
+              print("이전 화면이 없습니다.");
+            }
+          },
         ),
-        body: RestaurantListScreen(),
+        title: Text("내부 식당", style: textTheme.headlineMedium),
       ),
+      body: const RestaurantListScreen(),
     );
   }
 }
@@ -38,80 +30,36 @@ class RestaurantListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '내부 식당',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              '내부 식당',
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
           ),
-          SizedBox(height: 10),
-          SingleChildScrollView(
+        ),
+        SliverToBoxAdapter(
+          child: SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 RestaurantButton(name: '제순식당'),
-                SizedBox(width: 10),
+                const SizedBox(width: 10),
                 RestaurantButton(name: '치미치미'),
-                SizedBox(width: 10),
+                const SizedBox(width: 10),
                 RestaurantButton(name: '신역혼점'),
-                SizedBox(width: 10),
+                const SizedBox(width: 10),
                 RestaurantButton(name: '차이니스트'),
-                SizedBox(width: 10),
+                const SizedBox(width: 10),
                 RestaurantButton(name: '신의한컵'),
               ],
             ),
           ),
-          SizedBox(height: 20),
-          Text(
-            '제순식당',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('영업시작 시간', style: TextStyle(fontSize: 16)),
-              Text('10:00 AM',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-            ],
-          ),
-          SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('마감 시간', style: TextStyle(fontSize: 16)),
-              Text('8:00 PM',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-            ],
-          ),
-          SizedBox(height: 20),
-          Text(
-            '메뉴',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                ImagePlaceholder(name: '메뉴1'),
-                SizedBox(width: 10),
-                ImagePlaceholder(name: '메뉴2'),
-                SizedBox(width: 10),
-                ImagePlaceholder(name: '메뉴3'),
-                SizedBox(width: 10),
-                ImagePlaceholder(name: '메뉴4'),
-                SizedBox(width: 10),
-                ImagePlaceholder(name: '메뉴5'),
-              ],
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
@@ -123,18 +71,15 @@ class RestaurantButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 80, // 버튼 크기 지정
+      width: 80,
       child: Column(
         children: [
-          Transform.translate(
-            offset: const Offset(0, 0), // 동그라미 버튼을 위로 10px 올리기
-            child: CircleAvatar(
-              backgroundColor: Colors.grey[300], // 모든 버튼 동일 배경색
-              radius: 30,
-              child: const Icon(
-                Icons.restaurant, // 아이콘도 동일하게 설정
-                color: Colors.white,
-              ),
+          CircleAvatar(
+            backgroundColor: Colors.grey[300],
+            radius: 30,
+            child: const Icon(
+              Icons.restaurant,
+              color: Colors.white,
             ),
           ),
           const SizedBox(height: 5),
@@ -145,21 +90,6 @@ class RestaurantButton extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class ImagePlaceholder extends StatelessWidget {
-  final String name;
-  const ImagePlaceholder({super.key, required this.name});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      width: 150, // 메뉴 이미지 너비 추가
-      color: Colors.grey[200],
-      child: Center(child: Text('$name 사진')),
     );
   }
 }
