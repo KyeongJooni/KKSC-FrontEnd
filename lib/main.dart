04dc9cbb4a6e97652ff060a3b9748867b2c8fbab
dltmdgh0611 <dltmdgh0611@gmail.com>
2025-01-15 11:15:49 +0900

    안드로이드 빌드 문제 수정

--- lib/main.dart
+++ lib/main.dart
@@ -33,5 +33,49 @@ class MyApp extends StatelessWidget {
 }
 
 
+class IndexScreen extends StatefulWidget {
+  @override
+  _IndexScreenState createState() => _IndexScreenState();
+}
 
+class _IndexScreenState extends State<IndexScreen> {
+  int? _selectedIndex; // 초기값 null로 설정 (선택되지 않은 상태)
+
+  final List<Widget> _pages = [
+    TimetableScreen(),
+    HoneytipScreen(),
+    NoticeScreen(),
+    RestaurantScreen(),
+  ];
+
+  // 가천대 로고 클릭 시 초기화 동작
+  void _onLogoPressed() {
+    setState(() {
+      _selectedIndex = null; // 선택되지 않은 상태로 초기화
+    });
+  }
+
+  // 하단 네비게이션 아이템 선택 시 동작
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: GCG_TopAppbar(
+        onLogoPressed: _onLogoPressed, // 로고 클릭 시 호출
+      ),
+      body: _selectedIndex == null
+          ? MainpageScreen() // 선택되지 않은 상태에서는 MainPageScreen 표시
+          : _pages[_selectedIndex!], // 선택된 상태일 때 해당 페이지 표시
+      bottomNavigationBar: GCG_Bottomnavbar(
+        selectedIndex: _selectedIndex ?? -1, // 선택되지 않은 상태(-1)를 전달
+        onItemTapped: _onItemTapped,
+      ),
+    );
+  }
+}
 
